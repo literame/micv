916a0c57a6548f91020afbba0a38fd5a95584bbd
literame <litera.me.channel@gmail.com>
2023-02-07 21:02:22 -0500

    maquetado

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
     var isOpen = false;
+    $(".content").animate({marginLeft: "220px"});
     $(".sidebar").click(function() {
       if (isOpen) {
-        $(".sidebar").animate({width: "200px"});
-        $(".content").animate({marginLeft: "200px"});
-        $(".perfil").animate({display:"none"});
+        $(".sidebar").animate({width: "220px"});
+        $(".content").animate({marginLeft: "220px"});
+        $(".perfil .foto img").animate({width: "150px",height: "150px"});
+        $(".opt").animate({display: "block",fontsize:"22px"});
+
       } else {
-        $(".sidebar").animate({width: "50px"});
-        $(".content").animate({marginLeft: "50px"});
-        $(".perfil").animate({display:"flex"});
+        $(".sidebar").animate({width: "80px"});
+        $(".content").animate({marginLeft: "80px"});
+        $(".perfil .foto img").animate({width: "50px",height: "50px"});
+        $(".opt").animate({display: "none",fontsize:"10px"});
+
       }
       isOpen = !isOpen;
     });
